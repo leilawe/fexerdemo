09b6a0081133c029cee49020ff43834eb8370508
leilawe <weisileila@gmail.com>
2024-11-13 14:38:06 +0330

    bump to version 0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from setuptools import setup
  
  
 setup(name='fixer-demo',
-      version='0.1',
+      version='0.2',
       url="https://github.com/leilawe/fexerdemo",
+      install_requires=['requests'],
       packages=['fixer'],
  )
